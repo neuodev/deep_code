f689134bcc2ee6b2dbd6f09aaa831ae259635059
Ahmed Ibrahim <ib153507@gmail.com>
2022-11-15 07:38:03 +0200

    Add the problem section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen relative border-b-2 border-dashed mx-4">
       <h1 className="text-9xl mb-8">Deep Code</h1>
       <p className="text-2xl text-gray-300 mb-8">
         Enrich Arabic content for programmers
       </p>
 
-      <div className="bg-gray-800 border border-gray-100 rounded-lg p-6 italic shadow-2xl max-w-xl">
+      <div className="bg-gray-800 border border-gray-200 rounded-lg p-6 italic shadow-2xl max-w-xl">
         <p>
           Deep Code aims to fill the missing gap in the Arabic content related
           to programming. In the hope of making more people learn how to code
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Hero from "../components/Hero";
+import Problem from "../components/Problem";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-dark text-gray-100 font-rubik">
+      <Head>
+        <title>Deep Code - Code In Arabic </title>
+      </Head>
       <Hero />
+      <Problem />
     </div>
   );
 }
